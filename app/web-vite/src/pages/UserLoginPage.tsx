6eb96e8034afda5ba4e4af05d66775fd91a47a04
azlan <azlan@gaiadigitalagency.com>
2026-06-19 13:17:50 +0800

    fix: change user login page title to USER LOGIN

--- app/web-vite/src/pages/UserLoginPage.tsx
+++ app/web-vite/src/pages/UserLoginPage.tsx
@@ -12,7 +12,7 @@ export default function UserLoginPage() {
       />
       <LoginForm
         portal="user"
-        label="LOGIN"
+        label="USER LOGIN"
         defaultEmail="user@email.com"
         defaultPassword="User@123"
         emailLabel="USER ID"
